d5d1c5255e5f7e806635d12950c56d3289c4d6ae
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-01-16 17:37:04 -0500

    Modify the file comments to meet the recommended Elisp standards
    This will make it easier to submit bbcode-mode to various Elisp
    package repositories, which expect the file comments to follow the
    standard described in the official Elisp reference.

--- bbcode-mode.el
+++ bbcode-mode.el
@@ -1,39 +1,43 @@
 ;;; bbcode-mode.el --- Major mode for writing BBCode markup
-;;;
-;;; Copyright 2012, 2013 Eric James Michael Ritz
-;;;     <lobbyjones@gmail.com>
-;;;     <http://github.com/ejmr/bbcode-mode>
-;;;
-;;;
-;;;
+;;
+;; Copyright 2012, 2013 Eric James Michael Ritz
+;;
+;; Author: Eric James Michael Ritz <lobbyjones@gmail.com>
+;; URL: https://github.com/ejmr/bbcode-mode
+;; Version: 1.1.0
+;;
+;;
+;;
 ;;; License:
-;;;
-;;; This file is free software; you can redistribute it and/or modify
-;;; it under the terms of the GNU General Public License as published
-;;; by the Free Software Foundation; either version 3 of the License,
-;;; or (at your option) any later version.
-;;;
-;;; This file is distributed in the hope that it will be useful, but
-;;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;;; General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with this file; if not, write to the Free Software
-;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-;;; 02110-1301, USA.
-;;;
-;;;
-;;;
-;;; Usage:
-;;;
-;;; Put this file in your Emacs lisp path (i.e. site-lisp) and add
-;;; this to your `.emacs' file:
-;;;
-;;;     (require 'bbcode-mode)
-;;;
-;;; Files with the '.bbcode' extension automatically enable
-;;; bbcode-mode.  No other extensions are associated with the mode.
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation; either version 3 of the License,
+;; or (at your option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this file; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+;; 02110-1301, USA.
+;;
+;;
+;;
+;;; Commentary:
+;;
+;; Put this file in your Emacs lisp path (i.e. site-lisp) and add
+;; this to your `.emacs' file:
+;;
+;;     (require 'bbcode-mode)
+;;
+;; Files with the '.bbcode' extension automatically enable
+;; bbcode-mode.  No other extensions are associated with the mode.
+
+;;; Code:
 
 (defconst bbcode-mode-version-number "1.1.0"
   "BBCode Mode version number.")
@@ -161,3 +165,5 @@ buffer."
 (add-to-list 'auto-mode-alist '("\\.bbcode$" . bbcode-mode))
 
 (provide 'bbcode-mode)
+
+;;; bbcode-mode.el ends here
