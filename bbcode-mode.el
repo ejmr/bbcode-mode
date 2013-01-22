a7a494c1500671b2de3787edd5a2bd0cd90b876e
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-01-21 21:24:16 -0500

    Correctly update the version number to 1.1.2
    I mistakenly forgot to do this when creating the v1.1.2 tag.

--- bbcode-mode.el
+++ bbcode-mode.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Eric James Michael Ritz <lobbyjones@gmail.com>
 ;; URL: https://github.com/ejmr/bbcode-mode
-;; Version: 1.1.1
+;; Version: 1.1.2
 ;;
 ;;
 ;;
@@ -39,7 +39,7 @@
 
 ;;; Code:
 
-(defconst bbcode-mode-version-number "1.1.1"
+(defconst bbcode-mode-version-number "1.1.2"
   "BBCode Mode version number.")
 
 (defun bbcode/make-tag-regex (tag)
